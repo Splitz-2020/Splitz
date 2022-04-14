2d8a1528683e201c177220439c6433b049134a6b
“20it058 <20it058@charusat.edu.in>
2022-04-14 20:08:53 +0530

    First

---
+++ lib/Theme/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kBlackColour = Color(0xFF0A0E21);
+const kBackGroundColor = Color(0xFFba68c8);
+const kBlueColor = Color(0xFF18ffff);
+
+const kText = TextStyle(
+fontSize: 20.0,
+);
